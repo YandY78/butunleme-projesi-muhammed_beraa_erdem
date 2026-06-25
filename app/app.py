41076b2dd05ff78aed4862ee00487c4ebd66dafd
YandY78 <www.vengeful@gmail.com>
2026-06-25 03:52:50 +0300

    Saved model and feature columns

---
+++ app/app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Customer Churn Prediction",
+    layout="wide"
+)
+
+st.title("📊 Customer Churn Prediction System")
+
+st.write("Machine Learning Decision Support System")
